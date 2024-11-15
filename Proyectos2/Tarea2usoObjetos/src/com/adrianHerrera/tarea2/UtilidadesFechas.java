6f0f7979b550d41cbe804e78eded741f6022685f
Adrihm16 <ahermor2608@g.educaand.es>
2024-11-15 12:57:53 +0100

    actualizo todo

--- Proyectos2/Tarea2usoObjetos/src/com/adrianHerrera/tarea2/UtilidadesFechas.java
+++ Proyectos2/Tarea2usoObjetos/src/com/adrianHerrera/tarea2/UtilidadesFechas.java
@@ -2,13 +2,18 @@ package com.adrianHerrera.tarea2;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
-
 public class UtilidadesFechas {
-	/*devuelve la edad de una fecha en añs
+	/*
+	 * devuelve la edad de una fecha en años
+	 * 
 	 * @param fechaIntroducida
-	 * */
+	 * 
+	 * @return edad
+	 * 
+	 */
 	public static int calcularEdad(String fechaIntroducida) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 		int edad = 0;
@@ -23,5 +28,66 @@ public class UtilidadesFechas {
 			pe.printStackTrace();
 		}
 		return edad;
+	}
+
+	/*
+	 * Devuelve el mes del año en el que se encuentra la fecha introducida.
+	 * 
+	 * @param fechaIntroducida
+	 * 
+	 * @return mes
+	 * 
+	 */
+	public static String obtenerMes(String fechaIntroducida) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		String mes = "";
+		try {
+			Date fechaIntrDate = sdf.parse(fechaIntroducida);
+			Calendar fechaIntrCal = Calendar.getInstance();
+			fechaIntrCal.setTime(fechaIntrDate);
+			switch (fechaIntrCal.get(Calendar.MONTH)) {
+			case 0:
+				mes = "enero";
+				break;
+			case 1:
+				mes = "febrero";
+				break;
+			case 2:
+				mes = "marzo";
+				break;
+			case 3:
+				mes = "abril";
+				break;
+			case 4:
+				mes = "mayo";
+				break;
+			case 5:
+				mes = "junio";
+				break;
+			case 6:
+				mes = "julio";
+				break;
+			case 7:
+				mes = "agosto";
+				break;
+			case 8:
+				mes = "septiembre";
+				break;
+			case 9:
+				mes = "octubre";
+				break;
+			case 10:
+				mes = "noviembre";
+				break;
+			case 11:
+				mes = "diciembre";
+				break;
+//No uso default porque no es posible que aparezca otro valor (En el calendario gregoriano) 
+			}
+		} catch (ParseException e) {
+			System.err.println("El formato introducido no es correcto.");
+			e.printStackTrace();
+		}
+		return mes;
 	}
 }
--- Proyectos2/Tarea2usoObjetos/src/com/adrianHerrera/tarea2/pruebasTipoFecha.java
+++ Proyectos2/Tarea2usoObjetos/src/com/adrianHerrera/tarea2/pruebasTipoFecha.java
@@ -6,10 +6,33 @@ public class pruebasTipoFecha {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce una fecha en formato dd-mm-yyyy y te dire la edad de esa fecha");
-		String fechaIntroducida = sc.nextLine();
-		System.out.println(
-				"la edad de la fecha introducida es " + UtilidadesFechas.calcularEdad(fechaIntroducida) + " años.");
+		int opcion = 33;
+		do {
+			System.out.println("Introduce el número de la función que quieras probar: ");
+			opcion = sc.nextInt();
+			sc.nextLine();
+			switch (opcion) {
+			case 0:
+				System.out.println("Hasta pronto!");
+				break;
+			case 1:
+				System.out.println("Introduce una fecha en formato DD-MM-YYYY y te dire la edad de esa fecha: ");
+				String fechaIntroducida = sc.nextLine();
+				System.out.println("La edad de la fecha introducida es "
+						+ UtilidadesFechas.calcularEdad(fechaIntroducida) + " años.");
+				break;
+			case 2:
+				System.out.println(
+						"Introduce una fecha en formato DD-MM-YYYY y te diré el mes en el que esta se encuentra: ");
+				String fechaIntroducida1 = sc.nextLine();
+				System.out.println(
+						"El mes en el que se encuentra es " + UtilidadesFechas.obtenerMes(fechaIntroducida1) + ".");
+				break;
+			default:
+				System.out.println("Has elegido una opción incorrecta!");
+				break;
+			}
+		} while (opcion != 0);
 		sc.close();
 	}
 
